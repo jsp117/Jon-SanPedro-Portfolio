97b6cc4785514a7dda9c6bbad48e56a165c8823f
JonMac <jonsp14@gmail.com>
2020-10-07 19:35:34 -0700

    add playr html page, add styling and content to page

--- script.js
+++ script.js
@@ -8,11 +8,31 @@ $("#email").on("click", function(){
     window.location = "mailto:"+email+"?subject="+subject;
 });
 
-$(".btn-primary").on("click", function(){
+$(".btn-primary").on("click", function(event){
+event.preventDefault();
+    console.log($("#nameContact").val());
+    if($("#nameContact").val() == ""){
+        modal.attr("style", "display: block");
+        $("#modalText").text("Please enter your name");
+        return console.log("no text name");
+    }else if($("#emailContact").val() == ""){
+        modal.attr("style", "display: block");
+        $("#modalText").text("Please enter your email");
+        return console.log("no text email");
+    }else if($("#messageContact").val() == ""){
+        modal.attr("style", "display: block");
+        $("#modalText").text("Please enter a message");
+        return console.log("no text message");
+    }
     modal.attr("style", "display: block");
+    $("#modalText").text("Thank You!");
     var subject = 'Portfolio Site';
     var message = $("#messageContact").text();
-    window.location = "mailto:"+email+"?subject="+subject+"&body="+message;
+    // window.location = "mailto:"+email+"?subject="+subject+"&body="+message;
+    $("#nameContact").val("");
+    $("#emailContact").val("");
+    $("#messageContact").val("");
+
 });
 
 $("#close").on("click", function(){
